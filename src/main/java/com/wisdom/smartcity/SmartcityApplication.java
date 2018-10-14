73e1f04c52299de57a39ad80ea5fe45609d40b3a
leihenqingze <leihenqingze@sina.com>
2018-10-14 23:52:58 +0800

    第一次提交

---
+++ src/main/java/com/wisdom/smartcity/SmartcityApplication.java
@@ -0,0 +1,12 @@
+package com.wisdom.smartcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartcityApplication.class, args);
+    }
+}

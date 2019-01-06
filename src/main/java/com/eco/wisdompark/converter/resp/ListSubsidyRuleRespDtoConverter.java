c0f0cfa9273831464a7b92c72f390caf56457603
leihenqingze <leihenqingze@sina.com>
2019-01-06 11:40:35 +0800

    查询自动补助记录

--- src/main/java/com/eco/wisdompark/converter/resp/ListSubsidyRuleRespDtoConverter.java
+++ src/main/java/com/eco/wisdompark/converter/resp/ListSubsidyRuleRespDtoConverter.java
@@ -24,19 +24,31 @@ public class ListSubsidyRuleRespDtoConverter {
      * @param depts        补助规则对应的部门
      * @return 补助规则
      */
-    public static List<ListSubsidyRuleRespDto> converter(List<SubsidyRule> subsidyRules,
-                                                         Dept parent, List<Dept> depts) {
+    public static List<ListSubsidyRuleRespDto> converterList(List<SubsidyRule> subsidyRules,
+                                                             Dept parent, List<Dept> depts) {
         Map<Integer, Dept> deptMap = depts.stream()
                 .collect(Collectors.toMap(Dept::getId, a -> a, (k1, k2) -> k1));
         List<ListSubsidyRuleRespDto> listSubsidyRuleDtos =
-                subsidyRules.stream().map(subsidyRule -> {
-                    ListSubsidyRuleRespDto subsidyRuleDto = new ListSubsidyRuleRespDto();
-                    BeanUtils.copyProperties(subsidyRule, subsidyRuleDto);
-                    String deptName = parent.getDeptName() + "/" + deptMap.get(subsidyRule.getDeptId()).getDeptName();
-                    subsidyRuleDto.setDeptName(deptName);
-                    return subsidyRuleDto;
-                }).collect(Collectors.toList());
+                subsidyRules.stream().map(subsidyRule -> converter(subsidyRule, parent,
+                        deptMap.get(subsidyRule.getDeptId()))).collect(Collectors.toList());
         return listSubsidyRuleDtos;
+    }
+
+    /**
+     * 补助规则转换
+     *
+     * @param subsidyRule 补助规则
+     * @param parent      上级部门
+     * @param dept        补助规则对应的部门
+     * @return 补助规则
+     */
+    public static ListSubsidyRuleRespDto converter(SubsidyRule subsidyRule,
+                                                   Dept parent, Dept dept) {
+        ListSubsidyRuleRespDto subsidyRuleDto = new ListSubsidyRuleRespDto();
+        BeanUtils.copyProperties(subsidyRule, subsidyRuleDto);
+        String deptName = parent.getDeptName() + "/" + dept.getDeptName();
+        subsidyRuleDto.setDeptName(deptName);
+        return subsidyRuleDto;
     }
 
 }

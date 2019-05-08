6ba192c6065f6632be7573e7ce0eee696a2d4f11
leihenqingze <leihenqingze@sina.com>
2019-05-09 01:40:58 +0800

    数据库配置修改

--- src/main/java/com/eco/wisdompark/WisdomparkApplication.java
+++ src/main/java/com/eco/wisdompark/WisdomparkApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan("com.eco.wisdompark.mapper")
 @Configuration
 @EnableScheduling
+@EnableTransactionManagement
 public class WisdomparkApplication {
     public static void main(String[] args) {
         SpringApplication.run(WisdomparkApplication.class, args);
